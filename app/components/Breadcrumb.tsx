9133ca89e33b945fab143dadfc92f3fbf902f2fa
EZ ZERQTI RYAN <ryan.ez-zerqti@allianz.fr>
2025-01-02 13:58:54 +0100

    fix lint

--- app/components/Breadcrumb.tsx
+++ app/components/Breadcrumb.tsx
@@ -2,49 +2,43 @@ import { usePathname } from "next/navigation"
 import Paragraph from "./UI/Paragraph"
 import Link from "next/link"
 
-
 const Breadcrumb = () => {
+  const pathname = usePathname()
+  const paths = pathname?.split("/")
 
-    const pathname = usePathname()
-    const paths = pathname?.split('/')
+  if (!pathname || !paths?.length) return null
 
-    if (!pathname || !paths?.length) return null
+  const getLink = (i: number): string => {
+    if (!i) return "/"
+    let link = ""
+    link = paths
+      ?.slice(0, i + 1)
+      ?.map((p) => link + p)
+      .join("/")
+    return link
+  }
 
-    const getLink = (i: number): string => {
-        if (!i) return '/'
-        let link = '';
-        link = paths?.slice(0, i+1)?.map(p => link + p).join('/');
-        return link;
-    }
+  const getCleanPath = (path: string): string => {
+    if (!path) return "Acceuil"
+    return path.replaceAll("-", " ")
+  }
 
-    const getCleanPath = (path: string): string => {
-        if (!path) return 'Acceuil'
-        return path.replaceAll('-', ' ')
-    }
-
-    return (
-        <>
-            <ul className="flex flex-row gap-1">
-                {
-                    paths?.map((path, index) => (
-                        <li 
-                            key={index}
-                            className="hover:underline"
-                        >
-                            <Link
-                                href={getLink(index)}
-                            >
-                                <Paragraph 
-                                    text={ '> ' + getCleanPath(path)}
-                                    addStyle={`${index === paths?.length && 'font-bold'}`}
-                                />
-                            </Link>
-                        </li> 
-                    ))
-                }
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <ul className="flex flex-row gap-1">
+        {paths?.map((path, index) => (
+          <li key={index} className="hover:underline">
+            <Link href={getLink(index)}>
+              <Paragraph
+                text={"> " + getCleanPath(path)}
+                addStyle={`${index === paths?.length && "font-bold"}`}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default Breadcrumb
--- app/components/UI/Paragraph.tsx
+++ app/components/UI/Paragraph.tsx
@@ -1,17 +1,12 @@
 import { FC } from "react"
 
 type IProps = {
-    text: string
-    addStyle?: string
+  text: string
+  addStyle?: string
 }
 
-const Paragraph: FC<IProps> = ({text, addStyle}) => {
-
-    return (
-        <p className={`text-sm ${addStyle}`}>
-            {text}
-        </p>
-    )
+const Paragraph: FC<IProps> = ({ text, addStyle }) => {
+  return <p className={`text-sm ${addStyle}`}>{text}</p>
 }
 
 export default Paragraph
