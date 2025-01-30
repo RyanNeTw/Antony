d6c9a2f5d64b51440250ab93b7ccc0106921beae
NicolasNgu23 <nicolasnguyen0023@hotmail.com>
2025-01-30 23:13:49 +0100

    done

--- app/(web-pages)/signalements/page.tsx
+++ app/(web-pages)/signalements/page.tsx
@@ -6,6 +6,7 @@ import Header from "@/app/dashboard_agent/components/Header"
 import Breadcrumb from "@/app/components/Breadcrumb"
 import Footer from "@/app/dashboard_agent/Footer"
 import Signalements from "@/app/components/Signalements"
+import UrgentForm from "@/app/ui/signalements/urgentForm"
 
 const SignalementPage = () => {
   return (
@@ -17,7 +18,7 @@ const SignalementPage = () => {
             <Breadcrumb />
             <div className="flex">
               <Signalements />
-              <Form />
+              <UrgentForm />
             </div>
           </div>
           <Footer />
